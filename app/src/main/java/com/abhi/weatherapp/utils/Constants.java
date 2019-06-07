e4eae4f7a7827b6e3abcea1c18f1aa4cffb6abeb
abhinav gour <abhinavgr007@hotmail.com>
2019-06-07 18:37:10 +0530

    initial commit

---
+++ app/src/main/java/com/abhi/weatherapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.abhi.weatherapp.utils;
+
+public class Constants
+{
+    static String key ="5c9662ec07b14d2aafe125742190706";
+    static String url = "http://api.apixu.com/v1/current.json?key=" +key+ "&q=";
+
+}
