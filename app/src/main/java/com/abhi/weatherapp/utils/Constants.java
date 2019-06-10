63dac1e5c8aa4ba5d8b725c4fe8f636fefe3a487
Abhinav Gaur <abhinavgr007@hotmail.com>
2019-06-10 12:59:47 +0530

    Update Constants.java

--- app/src/main/java/com/abhi/weatherapp/utils/Constants.java
+++ app/src/main/java/com/abhi/weatherapp/utils/Constants.java
@@ -2,7 +2,7 @@ package com.abhi.weatherapp.utils;
 
 public class Constants
 {
-   public static String key ="88208720490de85931ed5779825ceb67";
+   public static String key ="88208720490de85931ed57798*****";
     public static String BASEURL = "https://api.openweathermap.org";
 
 }
